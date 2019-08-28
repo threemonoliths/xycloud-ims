0f01a7751e210c23aa7f71765ee1deacf23d8c10
tms <threemonoliths@gmail.com>
2019-08-28 16:49:10 +0800

    修改logo

--- admin-site/src/app/layout/default/header/components/user.component.ts
+++ admin-site/src/app/layout/default/header/components/user.component.ts
@@ -13,23 +13,11 @@ import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
       [nzDropdownMenu]="userMenu"
     >
       <nz-avatar [nzSrc]="settings.user.avatar" nzSize="small" class="mr-sm"></nz-avatar>
-      {{ settings.user.name }}
+      {{ username }}
     </div>
     <nz-dropdown-menu #userMenu="nzDropdownMenu">
       <div nz-menu class="width-sm">
-        <div nz-menu-item routerLink="/pro/account/center">
-          <i nz-icon nzType="user" class="mr-sm"></i>
-          {{ 'menu.account.center' | translate }}
-        </div>
-        <div nz-menu-item routerLink="/pro/account/settings">
-          <i nz-icon nzType="setting" class="mr-sm"></i>
-          {{ 'menu.account.settings' | translate }}
-        </div>
-        <div nz-menu-item routerLink="/exception/trigger">
-          <i nz-icon nzType="close-circle" class="mr-sm"></i>
-          {{ 'menu.account.trigger' | translate }}
-        </div>
-        <li nz-menu-divider></li>
+       
         <div nz-menu-item (click)="logout()">
           <i nz-icon nzType="logout" class="mr-sm"></i>
           {{ 'menu.account.logout' | translate }}
@@ -40,11 +28,16 @@ import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderUserComponent {
+
+  username: string = null
+
   constructor(
     public settings: SettingsService,
     private router: Router,
     @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
-  ) {}
+  ) {
+    this.username = localStorage.getItem("username")
+  }
 
   logout() {
     this.tokenService.clear();
