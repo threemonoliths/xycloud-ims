c53999bed7899dd0f88fad3f469da12c7c76e43d
tms <threemonoliths@gmail.com>
2019-12-03 10:05:49 +0800

    1

--- admin-site/src/app/shared/app-config.ts
+++ admin-site/src/app/shared/app-config.ts
@@ -1,2 +1,5 @@
+// export const baseUrl = 'http://172.27.39.116:4000/api/v1/';
+// export const wsConnStr = 'ws://172.27.39.116:4000/socket';
+
 export const baseUrl = 'http://localhost:4000/api/v1/';
 export const wsConnStr = 'ws://localhost:4000/socket';
