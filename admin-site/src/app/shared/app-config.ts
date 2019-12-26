29d6bac71e7dcb13a406ddb9f1b14ba8b771fb73
tms <threemonoliths@gmail.com>
2019-12-26 17:32:40 +0800

    前台URL

--- admin-site/src/app/shared/app-config.ts
+++ admin-site/src/app/shared/app-config.ts
@@ -1,5 +1,6 @@
 // export const baseUrl = 'http://172.27.39.116:4000/api/v1/';
 // export const wsConnStr = 'ws://172.27.39.116:4000/socket';
 
-export const baseUrl = 'http://localhost:4000/api/v1/';
+// export const baseUrl = 'http://localhost:4000/api/v1/';
+export const baseUrl = 'http://xycloud-ims-api-srv:4000/api/v1/';
 export const wsConnStr = 'ws://localhost:4000/socket';
